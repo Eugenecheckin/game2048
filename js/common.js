fad8bed3b93b411adf32f05adfcdb2dd5e806586
EvgeniyTTI <evgeniytti@rambler.ru>
2021-12-21 13:41:03 +0300

    addMoveStr_Common.js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 (function(window){
-	window.getNxtNumber = /*$('button').click*/(function(){
+	
+	window.getNxtNumber = (function(){
   	var arr = [ 
   	{ 
     	title: "2",
@@ -11,19 +12,30 @@
   	var rand = Math.random();
   	for (var i = 0; i < arr.length; i++) {
     	if (rand < arr[i].r) {
-      	 return arr[i].title;
+      	return arr[i].title;
       	break;
     	}
   	}
-});
-	window.StrCollapseButton = (function(arr, direct){
-		if (direct){
-			return arr.reduce(()=>{
-			})
-		}
-		else{}			
 	});
-
-
-
+	/*["0","0","1","1","0","1"]*/
+	window.getMoveStr = function(arr_str){
+		let preRes = [];
+		arr_str.forEach((element, i, arr) => {
+			if (element!=0){
+			preRes.push(arr[i]);
+			}					
+		})		
+		if (preRes.length==0){return ["0","0","0","0"]}
+		preRes.forEach((el,i,arr)=>{
+			if(el==arr[i+1]){
+			arr[i]=el*2;
+			arr[i+1]=0
+			}
+		})
+		let res = preRes.filter(item=>item>0)
+		while(res.length<4){
+		res.unshift("0");
+		}
+	return res;
+	}
 })(window);
